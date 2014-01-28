0ff00922a0ad1333616399dcf0c13992e195e04a
samuel flores <samuelflores@gmail.com>
2014-01-28 18:10:31 +0100

    progress

--- tests/01_compare/go.py
+++ tests/01_compare/go.py
@@ -1,16 +1,23 @@
-from Variable import Variable
+import unittest
+
 from src.readers.MAXCSVReader import MAXCSVReader
 
 __author__ = 'saflores'
 
-print ('hello...')
+class ReadTest(unittest.TestCase):
 
-r = MAXCSVReader('ref_214.csv')
+    def test_Read(self):
+        print ('hello...')
 
-for v in r.variables:
-    print(v)
-print()
-myVar = r.get_variable('CPC_SACU\SACU_p_delta_sensor_out_psid')
+        r = MAXCSVReader('ref_214.csv')
 
-for t in r.times:
-    print('{:03.2f} '.format(t), myVar.samples.get(t))
+        for v in r.variables:
+            print(v)
+        print()
+        myVar = r.get_variable('CPC_SACU\SACU_p_delta_sensor_out_psid')
+
+        for t in r.times:
+            print('{:03.2f} '.format(t), myVar.samples.get(t))
+
+if __name__ == '__main__':
+    unittest.main()
