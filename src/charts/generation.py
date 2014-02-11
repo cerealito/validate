02ed798cc1f5f4051a643dcba43c0f9232b5ca0a
samuel flores <samuelflores@gmail.com>
2014-02-11 17:29:49 +0100

    ...

--- src/charts/generation.py
+++ src/charts/generation.py
@@ -1,3 +1,5 @@
+from pip.vendor.html5lib.filters import alphabeticalattributes
+
 __author__ = 'cerealito'
 import matplotlib.pyplot as plt
 from os.path import abspath, exists
@@ -10,17 +12,16 @@ def generate_png(test, ref):
              label='ref', linestyle='solid', linewidth=2.5)
     plt.plot(test.times, test.values, color='#FF1D00',
              label='test', linestyle='dashed', linewidth=1.5)
-    plt.ylabel(test.name)
+    plt.title(test.name)
+    plt.ylabel('value')
     plt.xlabel('seconds')
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True)
+    plt.legend(ncol=1, loc=2)
 
+    # get the min and max values in the Y axis
     ymin, ymax = plt.ylim()
-    plt.ylim(ymax=ymax*1.1)
+    # set the y axis to be drawn 30% more than the default
+    plt.ylim(ymax=ymax*1.3)
 
-    # Ajust the window with box legend
-    ax = plt.subplot(111)
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
 
     # for some weird reason we must call savefig before show, otherwise the output file is all white
     plt.savefig(output_f)
--- src/report_generators/PDFReport.py
+++ src/report_generators/PDFReport.py
@@ -1,4 +1,4 @@
-from os.path import abspath
+from os.path import abspath, basename
 from charts.generation import generate_png
 
 __author__ = 'cerealito'
@@ -7,24 +7,27 @@ from fpdf import FPDF
 
 class PDFReport(FPDF):
 
-    def __init__(self, result_summary):
+    def __init__(self, file_cmp_result):
         super().__init__(orientation='P', unit='mm', format='A4')
-        self.r_summary = result_summary
-        self.title = 'untitled'
+        self.file_cmp_result = file_cmp_result
+        self.title = 'Validation Report for ' + basename(file_cmp_result.file_test)
 
         self.alias_nb_pages()
         self.add_page()
         self.set_font('Arial', '', 12)
 
     def header(self):
+        # no header for page 1
+        if self.page_no() == 1:
+            return
+
         # Arial bold 15
-        self.set_font('Arial', 'B', 10)
-        # Move to the right
-        self.cell(80)
+        self.set_font('Arial', 'B', 6)
         # Title
-        self.cell(30,10, self.title , 1, 0, 'C')
+        self.cell(w=190, h=5, txt=self.title, border='B', ln=0, align='C')
         # Line break
         self.ln(20)
+
 
     # Page footer
     def footer(self):
@@ -36,23 +39,33 @@ class PDFReport(FPDF):
         self.cell(0, 10, 'Page '+str(self.page_no())+'/{nb}', 0, 0, 'C')
 
     def summary(self):
+        #######################################################################
+        # Big title in the first page
+        self.set_y(30)
+        self.set_font('Arial', 'B', 18)
+        self.cell(w=190, h=10, txt=self.title, ln=1, border=0, align='C')
 
-        self.multi_cell(w=0, h=5, txt='Test report for files:', border=0)
+        #######################################################################
+        # body of the report
+        self.set_font('Arial', 'B', 12)
         self.ln()
-        self.cell(w=0, h=5, txt=abspath(self.r_summary.file_test), border=0, ln=1)
-        self.cell(w=0, h=5, txt=abspath(self.r_summary.file_ref), border=0, ln=1)
+        self.ln()
+        self.multi_cell(w=0, h=5, txt='Validation report for files:', border=0)
+        self.ln()
+        self.cell(w=0, h=5, txt=abspath(self.file_cmp_result.file_test), border=0, ln=1)
+        self.cell(w=0, h=5, txt=abspath(self.file_cmp_result.file_ref), border=0, ln=1)
         self.ln()
 
-        self.cell(w=0, h=5, txt='Over all result is ' + str(self.r_summary.files_are_equal), border=0, ln=1)
+        self.cell(w=0, h=5, txt='Over all result is ' + str(self.file_cmp_result.files_are_equal), border=0, ln=1)
         self.ln()
 
         self.cell(w=0, h=5, txt='The following variables were compared:', border=0, ln=1)
         self.set_font('Courier', 'B', 12)
         self.cell(w=0, h=5, txt='{:<60} {:>}'.format('Variable', 'Error'), border=0, ln=1)
-        for r in self.r_summary.result_l:
+        for r in self.file_cmp_result.result_l:
             self.cell(w=0, h=5, txt=str(r), border=0, ln=1)
 
         # for every couple of variables, generate a png
-        for result in self.r_summary.result_l:
+        for result in self.file_cmp_result.result_l:
             tmp_img_f = generate_png(result.test_var, result.ref_var)
-            self.image(tmp_img_f, w=180 )
+            self.image(tmp_img_f, w=190 )
