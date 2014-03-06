2f969e031403cbf2e5259cf6c3b159ac67afbb0c
Samuel Flores <samuelflores@gmail.com>
2014-03-07 00:51:33 +0100

    yeah

--- thread_xp/usage1-0.py
+++ thread_xp/usage1-0.py
@@ -3,21 +3,28 @@ from PyQt5.QtCore import QThread, QCoreApplication
 import sys, time
 
 class CustomThread(QThread):
+    def __init__(self):
+        super().__init__()
+        print('     Constructor lives in', self.thread())
+        print('              executes in', self.currentThread())
+        print()
+
     def run(self):
-        for i in range(10):
-            print(str(i), 'from work thread:', self.currentThreadId())
+        print()
+        for i in range(3):
+            print(str(i), 'runf lives in:', self.thread())
+            print(str(i), ' executes  in:', self.currentThread())
+            print()
             time.sleep(1)
         return
 
 
 myApp = QCoreApplication(sys.argv)
-print("  From main thread:", QThread.currentThreadId())
-
+print("  main is running in ", QThread.currentThread())
+print("      myApp lives in ", myApp.thread())
 t = CustomThread()
-t.finished.connect(myApp.quit)
+t.finished.connect(t.quit)
 
 t.start()
 
 myApp.exec()
-
-
