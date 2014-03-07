ecb0690f6e4e58778e066ad3b7b62ab801a5b6f6
samuel flores <samuelflores@gmail.com>
2014-03-07 17:56:21 +0100

    some experiments with threads

---
+++ thread_xp/10_qthread_subclass.py
@@ -0,0 +1,39 @@
+
+from PyQt5.QtCore import QThread, QCoreApplication, QObject
+import sys, time
+
+class CustomObject(QObject):
+    def __init__(self, *args):
+        super().__init__(*args)
+        print()
+        print('custom object lives in', self.thread())
+        print('           executes in', QThread.currentThread())
+
+class CustomThread(QThread):
+    def __init__(self):
+        super().__init__()
+        print()
+        print('Custom Thread constructor lives in:', self.thread())
+        print('                       executes in:', self.currentThread())
+        print()
+
+    def run(self):
+        print()
+        print('Custom Thread.run() lives in:', self.thread())
+        print('                executes  in:', self.currentThread())
+        print()
+        return
+
+#######################################
+myApp = QCoreApplication(sys.argv)
+print("    main is running in ", QThread.currentThread())
+print("        myApp lives in ", myApp.thread())
+
+t = CustomThread()
+t.finished.connect(t.quit)
+t.finished.connect(myApp.quit)
+t.start()
+
+print('t started, executing app...')
+
+myApp.exec()
