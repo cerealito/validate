7600c6ecd5afa12d2d01b4d4cf019559b4b10360
Samuel Flores <samuelflores@gmail.com>
2014-03-12 21:20:27 +0100

    svg tests

---
+++ src/charts/svg.py
@@ -0,0 +1,38 @@
+__author__ = 'cerealito'
+import matplotlib
+matplotlib.use("svg")
+import matplotlib.pyplot as plt
+from os.path import abspath, exists
+
+
+def generate_svg(test, ref):
+
+    output_f = test.name + '.svg'
+
+    plt.plot(ref.times(), ref.values(), color='#00FF21',
+             label='ref', linestyle='solid', linewidth=2.5)
+    plt.plot(test.times(), test.values(), color='#FF1D00',
+             label='test', linestyle='dashed', linewidth=1.5)
+    plt.title(test.name)
+    plt.ylabel('value')
+    plt.xlabel('seconds')
+    # one
+    plt.legend(ncol=1, loc=1)
+
+    # get the min and max values in the Y axis
+    ymin, ymax = plt.ylim()
+    # set the y axis to be drawn 30% more than the default
+    plt.ylim(ymax=ymax*1.3)
+
+
+    # for some weird reason we must call savefig before show, otherwise the output file is all white
+    # 240 for very high quality. 120 acceptable. 96 is fast but ugly
+    # TODO: make this configurable
+    plt.savefig(output_f, dpi=240, bbox_inches='tight')
+    # use clf instead of close, otherwise pyplot will crash in windows the second time!
+    # clearing somehow is necessary for pdf files with more than one chart
+    plt.clf()
+    if exists(output_f):
+        return abspath(output_f)
+    else:
+        return None
---
+++ tests/03_graph/test_svg.py
@@ -0,0 +1,46 @@
+import unittest
+from charts.svg import generate_svg
+
+from FileComparator import FileComparator
+
+__author__ = 'saflores'
+
+
+class GraphTester(unittest.TestCase):
+    def test_mini(self):
+        print('Comparing and graphing a mini error...')
+        t = '../00_install/small.csv'
+        r = '../00_install/small_2.csv'
+
+        fc = FileComparator(t, r)
+        res = fc.compare()
+
+        print('\n### Overall result is: ', res.is_acceptable)
+
+        # for every couple of variables, generate a png
+        for result in fc.result_couple_l:
+            of = generate_svg(result.test_var, result.ref_var)
+            self.assertIsNotNone(of)
+
+        self.assertFalse(res.is_acceptable)
+
+    #################################################################################
+    def test_diff(self):
+        print('Comparing to a known error...')
+        t = '../00_install/test_214.csv'
+        r = '../00_install/ref_214.csv'
+
+        fc = FileComparator(t, r)
+        res = fc.compare()
+
+        print('\n### Overall result is: ', res.is_acceptable)
+
+        # for every couple of variables, generate a png
+        for result in fc.result_couple_l:
+            of = generate_svg(result.test_var, result.ref_var)
+            self.assertIsNotNone(of)
+
+        self.assertTrue(res.is_acceptable)
+
+if __name__ == '__main__':
+    unittest.main()
