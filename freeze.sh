02394a5050f0ea77583112f1b8f92e43617486e0
samuel flores <samuelflores@gmail.com>
2014-03-21 16:58:50 +0100

    changes in the freeze script for win

--- freeze.sh
+++ freeze.sh
@@ -3,6 +3,8 @@ echo 'freezing...'
 freeze_cmd='cxfreeze validate.py -c --base-name=Win32GUI --target-dir dist --include-path=src --icon=ui/icons/account-logged-in.ico'
 $freeze_cmd
 echo
+echo copying extra files:
+cp -v C:/Python33/Lib/site-packages/PyQt5/libEGL.dll dist/
 
 echo erasing un-needed files:
 rm -v dist/_bz2.pyd
@@ -12,10 +14,6 @@ rm -v dist/_hashlib.pyd
 rm -v dist/_lzma.pyd
 rm -v dist/_multiprocessing.pyd
 rm -v dist/_ssl.pyd
-rm -v dist/icudt49.dll
-rm -v dist/icuin49.dll
-rm -v dist/icuuc49.dll
-rm -v dist/libGLESv2.dll
 rm -v dist/matplotlib.ttconv.pyd
 rm -v dist/numpy.core._dotblas.pyd
 rm -v dist/platforms/qminimal.dll
@@ -23,10 +21,17 @@ rm -v dist/platforms/qoffscreen.dll
 rm -v dist/pyexpat.pyd
 rm -v dist/select.pyd
 
-rm -vRf dist/imageformats/
-rm -vRf dist/mpl-data/fonts/
-rm -vRf dist/mpl-data/images/
-rm -vRf dist/mpl-data/sample_data/
+echo removing dist/imageformats/
+rm -Rf dist/imageformats/
+
+echo removing dist/mpl-data/fonts/
+rm -Rf dist/mpl-data/fonts/
+
+echo removing dist/mpl-data/images/
+rm -Rf dist/mpl-data/images/
+
+echo removing dist/mpl-data/sample_data/
+rm -Rf dist/mpl-data/sample_data/
 
 if [[ $? -eq 0 ]]; then
 	echo 'OK done';
